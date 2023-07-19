5f37344cdbd398f7b310a9b448cdb8a1bc176f98
NMEcheverria <echeverria.nicolas@hotmail.com>
2023-07-19 12:59:12 +0200

    Homework Prep Course Henry

---
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -0,0 +1,62 @@
+/*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
+/*2️⃣ EJERCICIO 02 2️⃣*/
+
+function devolverString(string) {
+   // Debe retornar un string.
+   // Tu código: 
+   return string;
+}
+
+// ⛔️ "X" e "Y" son números.
+
+function suma(x, y) {
+   // Retorna el resultado de su suma.
+   // Tu código: 
+   x === 4;
+   y === 9;
+   return x + y;
+   
+
+}
+
+function resta(x, y) {
+   // Retorna el resultado de la resta.
+   // Tu código:
+   x === 12
+   y === 5
+   return x - y;
+}
+
+function divide(x, y) {
+   // Retorna el resultado de su división.
+   // Tu código:
+   x === 40
+   y === 8
+   return x / y;
+}
+
+function multiplica(x, y) {
+   // Retorna el resultado de su multiplicación.
+   // Tu código:
+   x === 5
+   y === 3
+   return x * y;
+}
+
+function obtenerResto(x, y) {
+   // Obten el resto de la división de "x" entre "y".
+   // Tu código:
+   x === 50
+   y === 6
+   return x % y;
+}
+
+/*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
+module.exports = {
+   devolverString,
+   suma,
+   resta,
+   divide,
+   multiplica,
+   obtenerResto,
+};
